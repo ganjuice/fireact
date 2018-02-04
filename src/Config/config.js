9a5a9441a73130e8341c132d1807e64b306adecf
Amit Ganju <ganjuamit@gmail.com>
2018-02-03 22:41:18 -0600

    initial commit

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAsgfO0PfGxPVYa6G3iBJ2jxO8gMy2TNzo",
+    authDomain: "nomad-f508f.firebaseapp.com",
+    databaseURL: "https://nomad-f508f.firebaseio.com",
+    projectId: "nomad-f508f",
+    storageBucket: "nomad-f508f.appspot.com",
+    messagingSenderId: "749692404529"
+  };
